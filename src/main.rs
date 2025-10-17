f2a65cc4e23405b3de9a6f2767c2fb78e8bd3ddb
mysterious-diamond <aaronkenn10@gmail.com>
2025-10-17 20:02:44 +0700

    final commit

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,36 @@ use includes::*;
 
 #[derive(Template)]
 #[template(path="home.html")]
-struct HomePage;
+struct HomePage {
+	username: String,
+}
 
 #[derive(Template)]
 #[template(path="login.html")]
 struct LoginPage;
+
+#[derive(Deserialize)]
+struct LoginForm {
+	username: String,
+	password: String
+}
+
+#[tokio::main]
+async fn main() {
+	let store = MemoryStore::new();
+	let session_layer = SessionLayer::new(store, b"123".to_vec());
+
+    let app = Router::new()
+    	.route("/", get(home))
+    	.route("/login", get(login_get).post(_login_post))
+    	.layer(session_layer);
+
+	let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+	println!("Listening on http://{}", addr);
+	    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
 async fn home() -> Html<String> {
 	Html(HomePage.render().unwrap())
@@ -18,17 +43,4 @@ async fn login_get() -> Html<String> {
 }
 
 async fn _login_post() {
-}
-
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-    	.route("/", get(home))
-    	.route("/login", get(login_get).post(_login_post));
-
-	let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-	println!("Listening on http://{}", addr);
-	    
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
 }
